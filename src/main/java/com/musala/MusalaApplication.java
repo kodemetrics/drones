f09b00ca740138dc9f6a958b83a901d177a32dae
Kingsley Chukwuma Okafor <Kingsley.Okafor@abujaelectricity.com>
2021-12-27 22:21:46 +0100

    initialize_project

---
+++ src/main/java/com/musala/MusalaApplication.java
@@ -0,0 +1,32 @@
+package com.musala;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@SpringBootApplication
+public class MusalaApplication {
+
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext configurableApplicationContext =
+        SpringApplication.run(MusalaApplication.class, args);
+
+    }
+
+
+}
+
