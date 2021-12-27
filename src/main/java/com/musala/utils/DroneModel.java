6371763ed23e899a80e6bd4306180e8f71322329
Kingsley Chukwuma Okafor <Kingsley.Okafor@abujaelectricity.com>
2021-12-27 22:30:01 +0100

    create_utils

---
+++ src/main/java/com/musala/utils/DroneModel.java
@@ -0,0 +1,5 @@
+package com.musala.utils;
+
+public enum DroneModel {
+    Lightweight, Middleweight, Cruiserweight, Heavyweight
+}
---
+++ src/main/java/com/musala/utils/DroneState.java
@@ -0,0 +1,5 @@
+package com.musala.utils;
+
+public enum DroneState {
+    IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+}
